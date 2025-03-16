5a64a11886c416dc73a6f389de604c1d3bae4c6c
Pavel Permyakov <bageeer@gmail.com>
2025-03-16 21:36:15 +0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HistoryController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect()->route('login');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('history', [HistoryController::class, 'index'])->name('history');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
